6cfa90f6aa6f6264b38c4882f9fa25fa53b57450
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-09 21:08:36 -0400

    added if file exists

--- interpreter/main.c
+++ interpreter/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include "../src/include/io.h"
 //void Run(char* path[])
 //{
 	//char* pathEXT[] = path + ".juw";
@@ -13,6 +13,18 @@ int main(int argc, char* argv[])
 	if(strcmp(argv[1], "run") == 0 && argc == 3)
 	{
 		printf("Running %s", argv[2]);
+		if(FileExists(argv[3])== true)
+		{
+			printf("\033[1m\033[32m File Found. \033[0m");
+		}
+		else
+		{
+			printf("\033[1m\033[31m File Not Found Or Is Non-Accessable. \033[0m");
+
+		}
+	
+	}
+
 		//Run(argv[2])
 	}
 	else if (argc != 3)
--- src/io.c
+++ src/io.c
@@ -1,8 +1,17 @@
 #include "include/io.h"
 #include <stdlib.h>
 #include <stdio.h>
-bool FileExists(char* path[])
+bool FileExists(const char* fname)
 {
 	FILE* file;
+
+	if ((file = fopen(fname, 'rb')))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 	
 }

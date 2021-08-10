a3b9881a1b03916ecc0b1930bf8cdae72ac5c97e
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-09 21:01:45 -0400

    created io sys

---
+++ src/include/io.h
@@ -0,0 +1,10 @@
+#ifndef IO_H
+#define IO_H
+
+#include <cstdbool>
+
+bool FileExists(const char* fname);
+
+void ParseFile(const char* fname);
+
+#endif // !IO_H
---
+++ src/io.c
@@ -0,0 +1,8 @@
+#include "include/io.h"
+#include <stdlib.h>
+#include <stdio.h>
+bool FileExists(char* path[])
+{
+	FILE* file;
+	
+}
